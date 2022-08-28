47669a7ed458d4158bc5ab22a63a8d381bf84658
busra <doganbusra94@gmail.com>
2022-08-28 21:08:50 +0300

    improvements (code quality)

--- ChatService.Observer/Client.cs
+++ ChatService.Observer/Client.cs
@@ -12,21 +12,20 @@ namespace ChatService.Observer
     public class Client : IClient
     {
         private Socket _clientSocket;
+        private const string SERVER_HOST_NAME = "localhost";
         private readonly int _port = 3333;
         IPHostEntry host;
         IPAddress ipAddress;
         private Message _message = null;
+        private byte[] _bytes;
+
+        public List<string> MessageList = new List<string>();
 
         public Client()
         {
             try
-            {               
-                #region get remote end point
-                // get the remote ip address, it is used for establish connection, here localhost's ip : 127.0.0.1 
-                host = Dns.GetHostEntry("localhost");
-                ipAddress = host.AddressList[1];
-                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, _port);
-                #endregion
+            {
+                IPEndPoint remoteEndPoint = GetEndpoint();
 
                 // instantiating socket 
                 _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -61,23 +60,24 @@ namespace ChatService.Observer
                     string message = Console.ReadLine();
                     //string message = "Client sends message";
 
-                    if (message != null)
+                    
+                    if (!string.IsNullOrEmpty(message))
                     {
                         // convert message into byte array.
-                        _message.ByteMessage = Encoding.ASCII.GetBytes(message);
+                        _bytes = Encoding.ASCII.GetBytes(message);
 
                         // send message to server through socket
-                        _clientSocket.BeginSend(_message.ByteMessage, 0, _message.ByteMessage.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+                        _clientSocket.BeginSend(_bytes, 0, _bytes.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
 
                         //receive response from server
-                        //int bytesReceived = _clientSocket.Receive(_message.ByteMessage);
+                        //int bytesReceived = _clientSocket.Receive(_bytes);
 
                         //Console.WriteLine("Echo message = {0} ", Encoding.ASCII.GetString(bytes, 0, bytesReceived));
                     }
-                    else
-                    {
-                        Close();
-                    }
+                    //else
+                    //{
+                    //    Close();
+                    //}
                    // _message.MessageCount++;
 
                 }
@@ -88,10 +88,21 @@ namespace ChatService.Observer
                 throw new Exception("an error occurred while sending message! Exception: " + e.Message);
             }
         }
+       
         public void Close()
         {
             _clientSocket.Shutdown(SocketShutdown.Both);
             _clientSocket.Close();
+        }
+        private IPEndPoint GetEndpoint()
+        {
+            //get remote end point
+            // get the remote ip address, it is used for establish connection, here localhost's ip : 127.0.0.1 
+            host = Dns.GetHostEntry(SERVER_HOST_NAME);
+            ipAddress = host.AddressList[1];
+
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, _port);
+            return endPoint;  
         }
         private void ConnectCallback(IAsyncResult asyncResult)
         {
@@ -116,6 +127,20 @@ namespace ChatService.Observer
             catch (Exception e)
             {
                 throw new Exception("an error occurred while sending message! Exception: " + e.Message);
+
+            }
+        } 
+        private string Message(int i)
+        {
+            string message = (i + 1) + ". message";
+            MessageList.Add(message);
+
+            return message;
+        }
+        private void UpdateList()
+        {
+            for (int i = 0; i < length; i++)
+            {
 
             }
         }
--- ChatService.Observer/Server.cs
+++ ChatService.Observer/Server.cs
@@ -12,11 +12,13 @@ namespace ChatService.Observer
     public class Server : IServer
     {
         private Socket _serverSocket, _clientSocket;
+        private const string HOST_NAME = "localhost";
         private readonly int _port = 3333;
         private readonly int _maxRequestNumber = 10;
         IPHostEntry host;
         IPAddress ipAddress;
         private Message _message = null;
+        private byte[] _bytes;
         private DateTime _dateBeforeSend;
         private int _counterForReceiveMethodCall = 0;
 
@@ -28,12 +30,7 @@ namespace ChatService.Observer
         {
             try
             {
-                #region get  local end point for binding
-                // get host ip address, it is used for establish connection, here localhost's ip : 127.0.0.1 
-                host = Dns.GetHostEntry("localhost");
-                ipAddress = host.AddressList[1];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, _port);
-                #endregion
+                IPEndPoint localEndPoint = GetEndpoint();
 
                 // instantiating socket 
                 _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -74,10 +71,36 @@ namespace ChatService.Observer
 
             }
         }
+        public void NotifyClients()
+        {
+            GetCallCount();
+
+            if (_counterForReceiveMethodCall == 1)
+            {
+                //warn client
+                //SendWarningMessage();
+            }
+            else if (_counterForReceiveMethodCall > 1)
+            {
+                //shutdown client;
+                //_clientSocket.Close();
+
+            }
+        }
         public void Close()
         {
             _clientSocket.Shutdown(SocketShutdown.Both);
             _clientSocket.Close();
+        }
+        private IPEndPoint GetEndpoint()
+        {
+            //get local end point for binding
+            // get host ip address, it is used for establish connection, here localhost's ip : 127.0.0.1 
+            host = Dns.GetHostEntry(HOST_NAME);
+            ipAddress = host.AddressList[1];
+            IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, _port);
+
+            return iPEndPoint;
         }
 
         // when client connects to the server this method is called
@@ -93,7 +116,7 @@ namespace ChatService.Observer
 
                 _message = new Message();
 
-                _message.ByteMessage = new byte[_clientSocket.ReceiveBufferSize];
+                _bytes = new byte[_clientSocket.ReceiveBufferSize];
 
                 // TODO IF INTERVAL IS EQUAL OR LESS THAN 1 SEC, WARN THE CLIENT. IF ("WARNING_COUNT" > 1) THEN SHUTDOWN THE CLIENT
                 // UNIT TEST : 
@@ -102,9 +125,9 @@ namespace ChatService.Observer
 
                 // begin receiving data from client, put received data to _bytes
                 //once data is received, ReceiveCallback method is called  
-                _clientSocket.BeginReceive(_message.ByteMessage, 0, _message.ByteMessage.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), _clientSocket);
+                _clientSocket.BeginReceive(_bytes, 0, _bytes.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), _clientSocket);
                              
-
+                //accept from another/multiple client
                 Accept();
             }
             catch (Exception e)
@@ -123,13 +146,15 @@ namespace ChatService.Observer
                 SocketError error;
                 _clientSocket.EndReceive(asyncResult, out error);
 
+
                 if (error == SocketError.Success)
                 {
-                    _message.StringMessage = Encoding.ASCII.GetString(_message.ByteMessage);
+                    string message = Encoding.ASCII.GetString(_bytes);
 
-                    Console.WriteLine(_message.StringMessage);
+                    Console.Write(message);
 
-                    _clientSocket.BeginReceive(_message.ByteMessage, 0, _message.ByteMessage.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), _clientSocket);
+                    // receive messages from client continuously.
+                    _clientSocket.BeginReceive(_bytes, 0, _bytes.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), _clientSocket);
 
                     NotifyClients();
                     
@@ -161,22 +186,7 @@ namespace ChatService.Observer
             return _counterForReceiveMethodCall;
         }
 
-        public void NotifyClients()
-        {
-            GetCallCount();
-
-            if (_counterForReceiveMethodCall == 1)
-            {
-                //warn client
-                //SendWarningMessage();
-            }
-            else if (_counterForReceiveMethodCall > 1)
-            {
-                //shutdown client;
-                //_clientSocket.Close();
-
-            }
-        }
+        
 
         #region send message to client //not complete
         //public void SendWarningMessage()
@@ -190,16 +200,16 @@ namespace ChatService.Observer
         //            if (message != null)
         //            {
         //                // convert message into byte array.
-        //                _message.ByteMessage = Encoding.ASCII.GetBytes(message);
+        //                _bytes = Encoding.ASCII.GetBytes(message);
 
         //                // send message to client through socket
-        //                _serverSocket.BeginSend(_message.ByteMessage, 0, _message.ByteMessage.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+        //                _serverSocket.BeginSend(_bytes, 0, _bytes.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
 
         //            //receive response from client
 
-        //            int bytesReceived = _clientSocket.Receive(_message.ByteMessage);
+        //            int bytesReceived = _clientSocket.Receive(_bytes);
 
-        //            Console.WriteLine("Echo message = {0} ", Encoding.ASCII.GetString(_message.ByteMessage, 0, bytesReceived));
+        //            Console.WriteLine("Echo message = {0} ", Encoding.ASCII.GetString(_bytes, 0, bytesReceived));
         //        }
         //            else
         //            {
